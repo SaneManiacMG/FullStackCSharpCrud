d0d4b8839d962d1dc1ecc82e20d64a8f70a9e213
Mogomotsi Moroane <mmoroane@hotmail.com>
2023-03-14 22:20:37 +0200

    Created first getAll get method

---
+++ Backend.API/Controllers/EmployeesController.cs
@@ -0,0 +1,25 @@
+﻿using Backend.API.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Backend.API.Controllers
+{
+    [ApiController]
+    public class EmployeesController : Controller
+    {
+        private readonly FullStackDbContext _fullStackDbContext;
+
+        public EmployeesController(FullStackDbContext _fullStackDbContext)
+        {
+            this._fullStackDbContext = _fullStackDbContext;
+        }
+
+        [HttpGet]
+        [Route("api/[controller]")]
+        public async Task<IActionResult> GetAllEmployees()
+        {
+            var employees = await _fullStackDbContext.Employees.ToListAsync();
+            return Ok(employees);
+        }
+    }
+}
